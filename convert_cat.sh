90c6c96b4a6b58ac106fd297a451a18b2830d33f
Sayed Soroush Hashemi <hashemi.soroush@gmail.com>
2017-09-27 00:46:04 +0330

    Add multiple config capability
    Now user can set multiple configs in a single file.

--- convert_cat.sh
+++ convert_cat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tmp='.tmp/'
+
 while read -r line ; do
 	list=($line)
 	
@@ -13,21 +15,19 @@ while read -r line ; do
 	output_path=${list[$n]}
 	output_name=`basename "$output_path"`
 	concat_list_name="${output_name}_concat_list.txt"
-	concat_list_path="tmp/${concat_list_name}"
+	concat_list_path="${tmp}/${concat_list_name}"
 
-	mkdir tmp/
+	mkdir "${tmp}"
 	for i in `seq 0 $m` ; do
 		cur_out_name="part_${i}_$output_name" 
-		cur_out_path="tmp/$cur_out_name"
-		ffmpeg -i "${list[$i]}" -strict -2 -c:v libx264 "$cur_out_path"
-		sleep 3
-		echo "file '$cur_out_name'" >> "$concat_list_path"
+		cur_out_path="${tmp}/$cur_out_name"
+		ffmpeg -i "${list[$i]}" -strict -2 -c:v libx264 "$cur_out_path" < /dev/null
+		echo "file '$cur_out_name' " >> "$concat_list_path"
 	done
-	cd tmp
-	ffmpeg -f concat -i "$concat_list_name" -c copy "$output_name"
+	cd "${tmp}"
+	ffmpeg -f concat -i "$concat_list_name" -c copy "$output_name" < /dev/null
 	cd ..
-	mv "tmp/$output_name" "$output_path"
-	rm -rf tmp/
-	rm "$concat_list_path"
+	mv "${tmp}/$output_name" "$output_path"
+	rm -rf "${tmp}"
 
 done < "${1}"
