9b119e43f09365391ff190dae49a13b92f4f5318
Sayed Soroush Hashemi <hashemi.soroush@gmail.com>
2017-09-27 00:18:42 +0330

    Add convert_cat.sh that gets a config file and convert and concat videos according to it

---
+++ convert_cat.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while read -r line ; do
+	list=($line)
+	
+	n=0
+	for x in ${list[@]} ; do
+		let 'n++'
+	done
+	let 'n--'
+	let 'm=n-1'
+	
+	output_path=${list[$n]}
+	output_name=`basename "$output_path"`
+	concat_list_name="${output_name}_concat_list.txt"
+	concat_list_path="tmp/${concat_list_name}"
+
+	mkdir tmp/
+	for i in `seq 0 $m` ; do
+		cur_out_name="part_${i}_$output_name" 
+		cur_out_path="tmp/$cur_out_name"
+		ffmpeg -i "${list[$i]}" -strict -2 -c:v libx264 "$cur_out_path"
+		sleep 3
+		echo "file '$cur_out_name'" >> "$concat_list_path"
+	done
+	cd tmp
+	ffmpeg -f concat -i "$concat_list_name" -c copy "$output_name"
+	cd ..
+	mv "tmp/$output_name" "$output_path"
+	rm -rf tmp/
+	rm "$concat_list_path"
+
+done < "${1}"
